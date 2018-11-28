f018dad0401738cb47f77c9ea85261ff258f268b
Diogo Monteiro <diogo.monteiro@pitang.com>
2018-11-28 17:59:31 -0300

    Criando repository e service para categoria

---
+++ src/main/java/com/diogogm/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.diogogm.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.diogogm.cursomc.domain.Categoria;
+import com.diogogm.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
