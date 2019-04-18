1c369794681f40496c315dd93d833d299553bb9b
Diogo Monteiro <diogo.monteiro@pitang.com>
2019-04-18 15:55:53 -0300

    Validacao customizada: email nao repetido na atualizacao de Cliente

--- src/main/java/com/diogogm/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/diogogm/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.diogogm.cursomc.domain.Cliente;
+import com.diogogm.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 
 	private static final long serialVersionUID = 1L;
