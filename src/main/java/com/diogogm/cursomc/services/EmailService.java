f802216451a2ffa89a32c638afb8a2213e4204b4
Diogo Monteiro <diogo.monteiro@pitang.com>
2019-04-23 13:50:19 -0300

    Email HTML

--- src/main/java/com/diogogm/cursomc/services/EmailService.java
+++ src/main/java/com/diogogm/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.diogogm.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.diogogm.cursomc.domain.Pedido;
@@ -9,4 +11,8 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+
+	void sendHtmlEmail(MimeMessage msg);
 }
