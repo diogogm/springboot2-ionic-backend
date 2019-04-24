e06eb0d6e57d80d6ed569fc2de0739c6f7b1baa1
Diogo Monteiro <diogo.monteiro@pitang.com>
2019-04-24 13:07:08 -0300

    Restricao de conteudo: cliente so recupera ele mesmo

--- src/main/java/com/diogogm/cursomc/services/ClienteService.java
+++ src/main/java/com/diogogm/cursomc/services/ClienteService.java
@@ -15,11 +15,14 @@ import org.springframework.transaction.annotation.Transactional;
 import com.diogogm.cursomc.domain.Cidade;
 import com.diogogm.cursomc.domain.Cliente;
 import com.diogogm.cursomc.domain.Endereco;
+import com.diogogm.cursomc.domain.enums.Perfil;
 import com.diogogm.cursomc.domain.enums.TipoCliente;
 import com.diogogm.cursomc.dto.ClienteDTO;
 import com.diogogm.cursomc.dto.ClienteNewDTO;
 import com.diogogm.cursomc.repositories.ClienteRepository;
 import com.diogogm.cursomc.repositories.EnderecoRepository;
+import com.diogogm.cursomc.security.UserSS;
+import com.diogogm.cursomc.services.exceptions.AuthorizationException;
 import com.diogogm.cursomc.services.exceptions.DataIntegrityException;
 import com.diogogm.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -36,6 +39,12 @@ public class ClienteService {
 	private BCryptPasswordEncoder pe;
 	
 	public Cliente find(Integer id) {
+		
+		UserSS user = UserService.authenticated();
+		if (user==null || !user.hasRole(Perfil.ADMIN) && !id.equals(user.getId())) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 					"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()
---
+++ src/main/java/com/diogogm/cursomc/services/UserService.java
@@ -0,0 +1,17 @@
+package com.diogogm.cursomc.services;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import com.diogogm.cursomc.security.UserSS;
+
+public class UserService {
+	
+	public static UserSS authenticated() {
+		try {
+			return (UserSS) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		}
+		catch (Exception e) {
+			return null;
+		}
+	}
+}
