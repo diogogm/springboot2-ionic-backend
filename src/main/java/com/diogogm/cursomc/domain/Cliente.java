34d892df27c40edadc23294eeef092fec6e6f34f
Diogo Monteiro <diogo.monteiro@pitang.com>
2019-05-01 14:26:17 -0300

    Usando padrao de nomes para imagens

--- src/main/java/com/diogogm/cursomc/domain/Cliente.java
+++ src/main/java/com/diogogm/cursomc/domain/Cliente.java
@@ -48,7 +48,6 @@ public class Cliente implements Serializable{
 	@JsonIgnore
 	@OneToMany(mappedBy="cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
-	private String imageUrl;
 
 	public Cliente() {
 		addPerfil(Perfil.CLIENTE);
@@ -143,14 +142,6 @@ public class Cliente implements Serializable{
 
 	public void setPedidos(List<Pedido> pedidos) {
 		this.pedidos = pedidos;
-	}
-	
-	public String getImageUrl() {
-		return imageUrl;
-	}
-
-	public void setImageUrl(String imageUrl) {
-		this.imageUrl = imageUrl;
 	}
 	
 	@Override
